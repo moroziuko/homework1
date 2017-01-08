488b7841f91ea9ddaca6693ec883a87e5de9431f
Oksana Moroziuk <Oksana_Moroziuk@epam.com>
2017-01-08 15:43:10 +0200

    added css and js homeworks

---
+++ js_homework1_2/js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+function customPow() {
+    do {
+        var number = Number(prompt("Please enter a Number. Only integer digits are allowed: "));
+        var isNumberInteger = (typeof number === 'number') && (number % 1 === 0);
+    } while (!isNumberInteger)
+    do {
+        var exponent = Number(prompt("Please enter an Exponent. Only integer digits are allowed: "));
+        var isExponentInteger = (typeof exponent === 'number') && (exponent % 1 === 0);
+    } while (!isExponentInteger)
+
+    if (number === 0 || exponent === 0) {
+        console.log("Result:" + 0);
+    } else {
+        var result = number;
+        for (var index = 0; index < exponent - 1; index++) {
+            result = result * number;
+            console.log("Result:" + result);
+        }
+    }
+}
+
+console.log(customPow());
+
+
+
