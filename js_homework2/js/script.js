aca2d572832cf37563531a874e57762097dcbea1
Oksana Moroziuk <Oksana_Moroziuk@epam.com>
2017-01-08 16:13:12 +0200

    added js hw 2

--- js_homework2/js/script.js
+++ js_homework2/js/script.js
@@ -1,3 +1,22 @@
-/**
- * Created by oksanamoroziuk on 1/8/17.
- */
+"use strict";
+
+function checkNameAvailability() {
+    var namesArr = [prompt("Enter name 1: "), prompt("Enter name 2: "), prompt("Enter name 3: "), prompt("Enter name 4: "), prompt("Enter name 5: ")]
+    var userName = prompt("Enter user Name: ");
+    var isArrContainsUserName = false;
+
+    for (var index = 0; index < namesArr.length; index++) {
+        if (namesArr[index] == userName) {
+            isArrContainsUserName = true;
+            break;
+        }
+    }
+
+    if (isArrContainsUserName) {
+        alert(userName + ", you are successfully logged in.");
+    } else {
+        alert(userName + " name was not found.");
+    }
+}
+
+console.log(checkNameAvailability());
